b407b3ab6f4001bf4d1735385a1499e9922af3e8
XY <2732317515@qq.com>
2025-03-24 12:17:22 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentPerformanceAnalysisApp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // 定义 Exit_Click 方法
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            // 显示确认对话框
+            MessageBoxResult result = MessageBox.Show("确定要退出吗？", "退出", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                // 用户确认后关闭应用程序
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
